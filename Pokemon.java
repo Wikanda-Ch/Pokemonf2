2a6bad453cfd54f88460e59ad4346036c0979809
Wikanda-Ch <60845073+Wikanda-Ch@users.noreply.github.com>
2020-02-12 22:15:07 +0700

    Add files via upload

--- Pokemon.java
+++ Pokemon.java
@@ -1,5 +1,69 @@
-public class Pokemon {
+import java.util.Scanner;
 
-   int pokemon_boweiei;
+public class Pokemon {
+   Scanner sc = new Scanner(System.in);
+   private String name;
+   private int level;
+   private int hp;
+   //private String ansSleep;
+   //private String ansEat;
+
+   
+
+      public Pokemon(){
+         this.level = 1;
+         this.hp = 100;
+        // this.power = 100;
+        // this.exp = 0;
+      }
+        public void setName(){
+            System.out.print("Please Enter Your Pokemon Name:");
+            this.name = sc.nextLine();
+
+        }
+        public void getName(){
+            System.out.println(this.name);
+        }
+        public void setLevel(int level){
+            this.level = level;
+
+        }
+        public void getLevel(){
+            System.out.println(this.level);
+        }
+        public void setHp(int hp){
+            this.hp = hp;
+        }
+        public void getHp(){
+            System.out.println(this.hp);
+         }
+      
+    public void sleepAndEat(){
+        String ansSleep;
+        
+        System.out.println("Do you want to sleep?(Yes(Y) or No(N))");
+        ansSleep = sc.nextLine();
+            if(ansSleep.equals("Y"))
+                System.out.println("...zzZZZ");
+            else if
+            (ansSleep.equals("N")){
+                System.out.println("End Game, See you later");
+            }
+           
+    }
+    /*
+    public void eatBerry(int berry){
+        HP += 20;    
+    }
+     public void atk{
+        MP -= 5;
+     }
+     public void levelUp{
+
+     }
+     public void status{
+
+     }*/
+   
 
 }
--- Pokemongame.java
+++ Pokemongame.java
@@ -1,5 +1,26 @@
+
+
 public class Pokemongame{
-     public static void main (String[] args) {
-        System.out.println("----------------Start pokemon game------------------");
+     public static void main (String[] args){
+        Pokemon p1 = new Pokemon(); 
+        p1.setName();
+        Pokemongame runPkm = new Pokemongame();
+        runPkm.startPokemon(p1);
+        p1.sleepAndEat();
+        
+        
+        
+    
     }
+    public void startPokemon(Pokemon p){
+        System.out.println("----------------Start pokemon game------------------");
+        System.out.print("Name: ");
+            p.getName();
+        System.out.print("Level: ");
+            p.getLevel();
+        System.out.print("HP:");
+            p.getHp();
+        System.out.println("----------------------------------------------------");
+    }
+
 }
